bc20a835442e1c9d73ed58aaf0c17710eb6dc694
Tomas <stefancint@gmail.com>
2025-07-16 10:14:55 +0200

    Initial commit

--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('searchBar').addEventListener('input', function (e) {
-    const query = e.target.value.toLowerCase();
-    const books = document.querySelectorAll('.book');
-    books.forEach(book => {
-        const text = book.innerText.toLowerCase();
-        book.style.display = text.includes(query) ? 'flex' : 'none';
-    });
-});
